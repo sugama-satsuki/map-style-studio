f2bac7bd6ffbf32d1d2d5bcec0a9076a47d525ea
piguchi@geolonia.com <piguchi@geolonia.com>
2025-06-10 14:50:02 +0900

    styleダウンロードボタンを追加

--- src/components/LayerEditor/LayerEditor.tsx
+++ src/components/LayerEditor/LayerEditor.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Card, Flex, Layout, Typography } from 'antd';
+import { Button, Card, Flex, Layout, Typography } from 'antd';
 import './LayerEditor.css';
 import MapCanvas from '../MapCanvas/MapCanvas';
 import Sidebar from '../Sidebar/Sidebar';
 import { Header } from 'antd/es/layout/layout';
 import ColorPicker from '../ColorPicker/ColorPicker';
 import LayerList from '../LayerList/LayerList';
+import { useAtomValue } from 'jotai';
+import { styleAtom } from '../../atom';
 
 const { Sider, Content } = Layout;
 const { Title } = Typography;
@@ -14,6 +16,19 @@ const LayerEditor: React.FC = () => {
 
   // サイドバーの選択状態を管理
   const [selectedMenu, setSelectedMenu] = useState('layer');
+  const style = useAtomValue(styleAtom);
+
+  // style.jsonダウンロード処理
+  const handleDownloadStyleJson = () => {
+    if (!style) return;
+    const blob = new Blob([JSON.stringify(style, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'style.json';
+    a.click();
+    URL.revokeObjectURL(url);
+  };
 
   // サイドバー内で表示するコンポーネントを切り替え
   let sidebarContent = null;
@@ -25,8 +40,13 @@ const LayerEditor: React.FC = () => {
 
   return (
     <Layout className="layer-editor-root">
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <Title level={3} style={{color: '#fff'}}>アプリ名</Title>
+      <Header>
+        <Flex justify='space-between' align='center' style={{ width: '100%', height: '100%' }}>
+          <Title level={3} style={{color: '#fff', lineHeight: 1, margin: 0}}>map style studio</Title>
+          <Button type="primary" onClick={handleDownloadStyleJson}>
+            styleダウンロード
+          </Button>
+        </Flex>
       </Header>
       <Layout style={{ height: 'calc(100vh - 64px)' }}>
         <Sider
